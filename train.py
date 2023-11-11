c9c68d858b026dff80af01736ae7c45f8809a31a
GenitalKenobi <prashantss321@gmail.com>
2023-11-11 14:36:48 +0530

    first commit

--- train.py
+++ train.py
@@ -1,17 +1,18 @@
 from sklearn.datasets import load_iris
 from sklearn.svm import NuSVC
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 
 df = load_iris()
-X=df.data
-y=df.target
+X = df.data
+y = df.target
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 model = NuSVC()
 model.fit(X_train, y_train)
 
 y_pred = model.predict(X_test)
+metric = accuracy_score(y_test, y_pred)
 
-with open("results.txt", 'w') as f:
-    f.write("Model Mean Squared Error: {}".format(mean_squared_error(y_pred, y_test)))
+with open('result.txt', 'w') as f:
+    f.write("Model Accuracy: {}".format(metric))
